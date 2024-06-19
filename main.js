ef66a07da88c3b083c3f63143ec0f8d3482dd9ac
Bharath-kukkunoor <bharathkukkunoor@gmail.com>
2024-06-19 10:37:41 +0530

    small changes

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ window.onscroll = () => {
         let id = sec.getAttribute('id');
 
         if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
+            navLinks.forEach.apply(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
